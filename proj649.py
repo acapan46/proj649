4874e8129e2643456caa462d2da08813715f8159
acapan46 <108572412+acapan46@users.noreply.github.com>
2023-07-28 07:55:12 +0800

    Update proj649.py

--- proj649.py
+++ proj649.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-pip install scikit-learn
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
